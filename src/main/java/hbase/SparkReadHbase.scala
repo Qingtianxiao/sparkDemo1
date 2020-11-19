d8d0421759fadc5bbc0d9ba71bf9c4b0e1e70307
Qingtianxiao <1131793010@qq.com>
2020-11-19 20:23:55 +0800

    test

---
+++ src/main/java/hbase/SparkReadHbase.scala
@@ -0,0 +1,10 @@
+package hbase
+
+/**
+ * Created by ligc on 2020/11/19 20:18
+ */
+object SparkReadHbase {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ligc on 2020/11/19 20:13
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
